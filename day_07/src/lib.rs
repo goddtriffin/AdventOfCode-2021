5221d8b3de66b89cd75f6ad9c3e0218e3748abcc
Todd Everett Griffin <goddtriffin@gmail.com>
2021-12-20 13:40:30 -0600

    Complete Day 7 part 2.

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -14,14 +14,46 @@ pub fn part_one(input: &Vec<usize>) -> usize {
     let mid = input.len() / 2;
     let least_fuel_position = input[mid];
 
+    calculate_total_fuel(input, least_fuel_position, &linear_fuel_cost)
+}
+
+pub fn part_two(input: &Vec<usize>) -> usize {
+    let least_fuel_position =
+        (input.iter().sum::<usize>() as f32 / input.len() as f32).floor() as usize;
+
+    let floor_total_fuel = calculate_total_fuel(input, least_fuel_position, &quadratic_fuel_cost);
+    let ceil_total_fuel =
+        calculate_total_fuel(input, least_fuel_position + 1, &quadratic_fuel_cost);
+
+    if floor_total_fuel < ceil_total_fuel {
+        floor_total_fuel
+    } else {
+        ceil_total_fuel
+    }
+}
+
+pub fn calculate_total_fuel(
+    input: &Vec<usize>,
+    least_fuel_position: usize,
+    cost_function: &dyn Fn(usize, usize) -> usize,
+) -> usize {
     let mut total_fuel = 0;
     for position in input.iter() {
-        total_fuel += ((*position as isize) - (least_fuel_position as isize)).abs();
+        total_fuel += cost_function(least_fuel_position, *position);
     }
 
     total_fuel as usize
 }
 
-pub fn part_two(_input: &Vec<usize>) -> usize {
-    2
+pub fn linear_fuel_cost(least_fuel_position: usize, horizontal_position: usize) -> usize {
+    position_difference(least_fuel_position, horizontal_position)
+}
+
+pub fn quadratic_fuel_cost(least_fuel_position: usize, horizontal_position: usize) -> usize {
+    let difference = position_difference(least_fuel_position, horizontal_position);
+    (difference * (difference + 1)) / 2
+}
+
+pub fn position_difference(least_fuel_position: usize, horizontal_position: usize) -> usize {
+    ((horizontal_position as isize) - (least_fuel_position as isize)).abs() as usize
 }
--- day_07/tests/lib.rs
+++ day_07/tests/lib.rs
@@ -1,4 +1,4 @@
-use day_07::part_one;
+use day_07::{part_one, part_two};
 use utils::io::lines_from_file;
 
 #[test]
@@ -31,4 +31,36 @@ fn d7_p1_real() {
     input.sort();
 
     assert_eq!(364898, part_one(&input));
+}
+
+#[test]
+fn d7_p2_t1() {
+    let input: Vec<String> = lines_from_file("d7t1.txt")
+        .unwrap()
+        .iter()
+        .map(|s| s.parse::<String>().unwrap())
+        .collect();
+    let mut input: Vec<usize> = input[0]
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+    input.sort();
+
+    assert_eq!(168, part_two(&input));
+}
+
+#[test]
+fn d7_p2_real() {
+    let input: Vec<String> = lines_from_file("d7.txt")
+        .unwrap()
+        .iter()
+        .map(|s| s.parse::<String>().unwrap())
+        .collect();
+    let mut input: Vec<usize> = input[0]
+        .split(",")
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+    input.sort();
+
+    assert_eq!(104149091, part_two(&input));
 }
