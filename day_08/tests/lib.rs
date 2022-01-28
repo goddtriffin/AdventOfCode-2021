981aa9b60fe40c985e2af7e87210afaa16576ea6
Todd Everett Griffin <goddtriffin@gmail.com>
2022-01-27 22:06:11 -0800

    Intermediate progress.

--- day_08/tests/lib.rs
+++ day_08/tests/lib.rs
@@ -1,4 +1,4 @@
-use day_08::part_one;
+use day_08::{part_one, part_two};
 use utils::io::lines_from_file;
 
 #[test]
@@ -10,4 +10,26 @@ fn d8_p1_t1() {
         .collect();
 
     assert_eq!(26, part_one(&input));
+}
+
+#[test]
+fn d8_p1_real() {
+    let input: Vec<String> = lines_from_file("d8.txt")
+        .unwrap()
+        .iter()
+        .map(|s| s.parse::<String>().unwrap())
+        .collect();
+
+    assert_eq!(534, part_one(&input));
+}
+
+#[test]
+fn d8_p2_t1() {
+    let input: Vec<String> = lines_from_file("d8t1.txt")
+        .unwrap()
+        .iter()
+        .map(|s| s.parse::<String>().unwrap())
+        .collect();
+
+    assert_eq!(61229, part_two(&input));
 }
